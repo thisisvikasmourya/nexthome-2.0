08700f16a4632bfc730fc41c07d77c5155135056
vikasmourya <60725905+thisisvikasmourya@users.noreply.github.com>
2024-02-01 10:25:16 +0530

    done

--- frontend_rental/src/App.jsx
+++ frontend_rental/src/App.jsx
@@ -1,10 +1,12 @@
 // import { useState } from 'react'
 import "./App.css";
+import Registration from "./components/Registration";
 
 function App() {
   return (
     <>
-      <h1>rental Property</h1>
+      {/* <h1>rental Property</h1> */}
+      <Registration />
     </>
   );
 }
