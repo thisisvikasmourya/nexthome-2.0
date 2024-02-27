afdce7dd95437d116ca808411b73140ae0ef6c4b
vikasmourya <60725905+thisisvikasmourya@users.noreply.github.com>
2024-02-27 23:02:41 +0530

    done

--- frontend_rental/src/features/auth/authSlice.js
+++ frontend_rental/src/features/auth/authSlice.js
@@ -13,6 +13,34 @@ export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {},
+  extraReducers: {
+    [userLogin.pending]: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    [userLogin.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.userInfo = payload;
+      state.userToken = payload.userToken;
+    },
+    [userLogin.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+    // register user
+    [registerUser.pending]: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    [registerUser.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.success = true; // registration successful
+    },
+    [registerUser.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+  },
 });
 
 export default authSlice.reducer;
